6962897ba0e1e71ac9085bf73b213628d79e3254
Adam Gonzales <damjee19@me.com>
2024-03-28 03:35:03 -0700

    added autosave

---
+++ lua/core/plugins/auto-save.lua
@@ -0,0 +1,11 @@
+local M = {
+	"Pocco81/auto-save.nvim",
+	config = function()
+		require("auto-save").setup({
+			-- your config goes here
+			-- or just leave it empty :)
+		})
+	end,
+}
+
+return M
