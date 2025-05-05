be17d33b55c6fe6de67ed2eb92059ef30aa5747e
Adam Gonzales <damjee19@me.com>
2025-05-04 20:51:20 -0700

    fixed treesitter issue on windows, removed copilot chat

--- lua/core/plugins/treesitter.lua
+++ lua/core/plugins/treesitter.lua
@@ -1,45 +1,24 @@
 local M = {
 	"nvim-treesitter/nvim-treesitter",
-	build = function()
-		require("nvim-treesitter.install").update({ with_sync = true })()
-	end,
+	build = ":TSUpdate",
 	config = function()
+		require("nvim-treesitter.install").prefer_git = false
+		require("nvim-treesitter.install").compilers = { "clang", "gcc" }
+
 		local configs = require("nvim-treesitter.configs")
 		configs.setup({
 			ensure_installed = {
-				"c",
-				"cpp",
-				"lua",
-				"vim",
-				"vimdoc",
-				"query",
-				"javascript",
-				"typescript",
-				"json",
-				"html",
-				"bash",
-				"fish",
-				"css",
-				"csv",
-				"dockerfile",
-				"gdscript",
-				"gdshader",
-				"godot_resource",
-				"go",
-				"gomod",
-				"gitignore",
-				"just",
-				"python",
-				"regex",
-				"rust",
-				"sql",
-				"tmux",
-				"toml",
-				"xml",
-				"yaml"
+				"c", "cpp", "lua", "vim", "vimdoc", "query",
+				"javascript", "typescript", "json", "html",
+				"bash", "fish", "css", "csv", "dockerfile",
+				"gdscript", "gdshader", "godot_resource",
+				"go", "gomod", "gitignore", "just",
+				"python", "regex", "rust", "sql", "tmux",
+				"toml", "xml", "yaml"
 			},
+			auto_install = true,
 			highlight = { enable = true },
-			--indent = { enable = true }
+			indent = { enable = true },
 		})
 	end,
 }
